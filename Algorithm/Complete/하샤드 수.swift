59e987e01ed9db69a2852920748a60ebb2a7613a
0Hooni <thddudgns972@gmail.com>
2022-10-11 21:49:53 +0900

    Update 하샤드 수.swift

--- Algorithm/Complete/하샤드 수.swift
+++ Algorithm/Complete/하샤드 수.swift
@@ -9,6 +9,6 @@ func solution(_ n:Int) -> Bool {
     return n%sum == 0
 }
 
-//func solution(_ x:Int) -> Bool {
-//    return x % String(x).reduce(0, {$0+Int(String($1))!}) == 0
-//}
+// func solution(_ x:Int) -> Bool {
+//     return x % String(x).reduce(0, {$0+Int(String($1))!}) == 0
+// }
